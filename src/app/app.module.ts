e5d621a15ab73fc72dd3ae8e5a7a7349b6b12bf1
singhtrustcom <78361510+singhtrustcom@users.noreply.github.com>
2022-05-01 17:00:36 +0200

    add template ans logic for game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './components/board/board.component';
+import { SquarComponent } from './components/squar/square.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    SquarComponent
   ],
   imports: [
     BrowserModule,
